c4c0ed23957e8230626e498e901f5e99739133fb
Lukas Rubikas <lukas.rubikas@gmail.com>
2014-10-09 15:39:22 +0300

    Trying out routes

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET users listing. */
 router.get('/', function(req, res) {
-  res.send('respond with a resource');
+    var simpleObject = {
+            "name": 'Lukas'
+        };
+    res.json(simpleObject);
 });
+
+/* GET /users/name */
+router.get('/:name', function(req, res) {
+    var name = req.params.name,
+        simpleObject = {
+            "name": name
+        };
+    res.json({'name': name});
+});
+
+exports.smth = function(req, res){
+    res.send('...');
+};
 
 module.exports = router;
